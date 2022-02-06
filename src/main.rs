e1671fcd876e08971d27423ad4d802fb56113dfc
thrizzle <73202594+terabyte3@users.noreply.github.com>
2022-02-06 07:48:28 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -64,7 +64,7 @@ async fn main() -> std::io::Result<()> {
             // .service(Files::new("/assets", "../templates/assets").show_files_listing())
         }
     )
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
         
